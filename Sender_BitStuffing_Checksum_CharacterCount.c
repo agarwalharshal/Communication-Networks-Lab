8a3382a11d660f18826493647c491962d5156dae
Harshal Agarwal <76912714+agarwalharshal@users.noreply.github.com>
2023-05-13 22:51:58 +0530

    Update Sender_BitStuffing_Checksum_CharacterCount.c

--- Sender_BitStuffing_Checksum_CharacterCount.c
+++ Sender_BitStuffing_Checksum_CharacterCount.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include <conio.h>
+#include <conio.h> //console input/output
 #include <math.h>
 #include <string.h>
 
 int i,j;
 
-void sender(int b[10],int k)
+void sender(int b[100],int k) //b is not a bit string but an array of integers where each integer works as a frame/subunit
 {
     int checksum,sum=0;
     printf("\n****SENDER****\n");
     for(i=0;i<k;i++)
         sum+=b[i];
     printf("SUM IS: %d",sum);
-    checksum=~sum;
+    checksum=~sum; //checksum = 1's complement of sum
     printf("\nSENDER's CHECKSUM IS:%d",checksum);
-    int errorDetection = ~(checksum+sum);
+    int errorDetection = ~(checksum+sum); //errorDetection = 1's complement of (checksum+sum)
     printf("\nERROR DETECTION:%d",errorDetection);
     if(errorDetection==0)
         printf("\n\nACCEPTED");
@@ -34,6 +34,17 @@ int main()
     switch(choice)
     {
         //Checksum Calculation
+            //m = 2
+            //10101001  --> a[0]=169
+            //00111001  --> a[1]=57
+            //11100010  --> sum
+            //00011101  --> checksum (1's complement of sum)
+            //Data transmitted to Receiver is – 10101001 00111001 00011101
+            //10101001  --> a[0]=169
+            //00111001  --> a[1]=57
+            //00011101  --> checksum
+            //11111111  --> sumNew = sum+checksum   sum=a[0]+a[1]
+            //00000000  --> errorDetection = 1's complement of sumNew   errorDetection=0 => no error
         case 'a':
         {
             printf("\nENTER SIZE OF THE STRING:");
@@ -44,7 +55,10 @@ int main()
             sender(a,m);
         }
         break;
+           
         //Bit stuffing
+            //110101111101011111101011111110    --> before bit stuffing
+            //110101111100101111101010111110110 --> after bit stuffing
         case 'b':
         {
             int count=0;
@@ -69,7 +83,12 @@ int main()
             printf("\nAfter Bit stuffing :  %s\n", bstr);
         }
         break;
+           
         //Inserting character count code
+            //"hello"   --> arr[]
+            //strlen(arr)+1   --> 5+1 = 6
+            //header = 6
+            //transmitted frame = 6hello (6 characters in the frame including the header character)
         case 'c':
         {
             char arr[100]; int x,y;
